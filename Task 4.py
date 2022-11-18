ad82b5c3e30979c9a509c99adc02f90c742e8953
andrmiw9 <34866926+andrmiw9@users.noreply.github.com>
2022-11-18 22:32:04 +0300

    Task 4 in progress

--- Task 4.py
+++ Task 4.py
@@ -1,25 +1,86 @@
 # (Code parser)
-data = {}
+
+data = {}  # данные по уровням
+
+
+def is_in_data_up(subj, cur_level):
+    for lvl in data:  # maybe add list slicing, if using of list type of data is chosen
+        if lvl > cur_level:  # skip if lvl is deeper, and yes im too lazy to rewrite 'in data + if' to simply 'i in
+            # range() + indexes'
+            continue
+        for entry in data[lvl]:
+            if subj == entry:
+                return lvl
+    return None
+
+
+def return_old_values(level_to_delete):
+    for
+    pass
+
+
+# find_in_data(5)
 
 with open('input.txt', 'r') as f:
+    cur_lvl: int = 0
+    data[0] = {}
+
     for line in f.readlines():
         line = line.rstrip()
-        print(line)
+        print('\n' + line)
 
         if line == '{':
-            pass
+            cur_lvl += 1
+            if cur_lvl not in data:
+                data[cur_lvl] = {}
         elif line == '}':
-            pass
+            del data[cur_lvl]  # delete local scope level
+            cur_lvl -= 1
+            if cur_lvl not in data:
+                data[cur_lvl] = {}
+            # (for list real-n)check if lvl is < 0, and if is, move data lvls, and delete last context based on cur_lvl
         else:
             line = line.split('=')
             try:  # число
                 line[1] = int(line[1])
+                print('Number')
 
-                data[line[0]] = line[1]
+                lvl = is_in_data_up(line[0], cur_lvl)
+                if lvl is not None:  # var in data
+                    data[lvl][line[0]] = line[1]
+                    # print(t)
+                    # t = line[1]
+                else:  # var not in data
+                    data[cur_lvl][line[0]] = line[1]
+                    # print(line[1])
+                # print(line[1])
+
             except Exception:  # переменная
-                
-                pass
+                print('Exception')
 
-    pass
+                lvl1 = is_in_data_up(line[0], cur_lvl)
+                lvl2 = is_in_data_up(line[1], cur_lvl)
 
-print(data)
+                if lvl1 is not None and lvl2 is not None:  # both vars are in data
+                    print('both is')
+                    data[lvl1][line[0]] = data[lvl2][line[1]]
+                    print(data[lvl1][line[0]])
+
+
+                elif lvl1 is not None and lvl2 is None:  # first var is in data, its level = lvl1, second is not
+                    print('lvl1 only')
+                    data[lvl1][line[0]] = 0
+                    print(0)
+
+                elif lvl1 is None and lvl2 is not None:  # second in data, first not
+                    print('lvl2 only')
+                    t = data[lvl2][line[1]]
+                    data[cur_lvl][line[0]] = t
+                    print(t)
+
+                else:  # both vars are not in data
+                    print('both not')
+                    data[cur_lvl][line[0]] = 0
+                    print(0)
+
+    print(data)
